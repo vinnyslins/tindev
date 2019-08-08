412b4510b3eb57a668947b19d1cfbcc2af072011
Vinnys Lins <vinnysfl@hotmail.com>
2019-08-08 00:49:25 -0300

    Added Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login';
+
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <Login/>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Login.css'
+
+import logo from '../assets/logo.svg';
+
+function Login() {
+  return (
+    <img src={logo} alt='tindev'/>
+  );
+}
+
+export default Login;
