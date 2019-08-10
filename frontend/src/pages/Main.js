dc6cac7b1f34c508e4efded1e65b76aec84502b4
Vinnys Lins <vinnysfl@hotmail.com>
2019-08-09 23:44:05 -0300

    Main page styles

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,81 @@
 import React from 'react';
 
+import './Main.css';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
 function Main({ match }) {
-  return <h1>{match.params.id}</h1>
+  return (
+    <div className='main-container'>
+      <img src={logo} alt='tindev'/>
+      <ul>
+        <li>
+          <img src='https://avatars3.githubusercontent.com/u/4248081?s=460&v=4' alt=''/>
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador PHP</p>
+          </footer>
+          <div className='buttons'>
+            <button>
+              <img src={dislike} alt='Dislike'></img>
+            </button>
+            <button>
+              <img src={like} alt='Like'></img>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src='https://avatars3.githubusercontent.com/u/4248081?s=460&v=4' alt=''/>
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador PHP</p>
+          </footer>
+          <div className='buttons'>
+            <button>
+              <img src={dislike} alt='Dislike'></img>
+            </button>
+            <button>
+              <img src={like} alt='Like'></img>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src='https://avatars3.githubusercontent.com/u/4248081?s=460&v=4' alt=''/>
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador PHP</p>
+          </footer>
+          <div className='buttons'>
+            <button>
+              <img src={dislike} alt='Dislike'></img>
+            </button>
+            <button>
+              <img src={like} alt='Like'></img>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src='https://avatars3.githubusercontent.com/u/4248081?s=460&v=4' alt=''/>
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador PHP</p>
+          </footer>
+          <div className='buttons'>
+            <button>
+              <img src={dislike} alt='Dislike'></img>
+            </button>
+            <button>
+              <img src={like} alt='Like'></img>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
